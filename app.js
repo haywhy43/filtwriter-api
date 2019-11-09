41087e0a889a231be9247781ca3c9d0e334a5d08
haywhy43 <170805005@live.unilag.edu.ng>
2019-11-09 19:06:28 +0100

    db update

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const upload = multer({ dest: "uploads/" });
 const db = knex({
     client: "pg",
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         user: 'peafngzxrmturs',
         database: 'd987vpb6s8jq8',
         ssl: true
