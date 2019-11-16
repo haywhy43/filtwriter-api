96f917d7179af1def5ec44bd1134da7ceb3c0740
haywhy43 <170805005@live.unilag.edu.ng>
2019-11-16 15:27:10 +0100

    changinf variables to push to live

--- app/db.js
+++ app/db.js
@@ -7,10 +7,10 @@ export default callback => {
     const db = knex({
         client: "pg",
         connection: {
-            host: process.env.DATABASE_URL,
-            user: "ayomikun",
-            database: "filtwriter-db",
-            // ssl: true
+            connectionString: process.env.DATABASE_URL,
+            // user: "ayomikun",
+            // database: "filtwriter-db",
+            ssl: true
         }
     });
 
