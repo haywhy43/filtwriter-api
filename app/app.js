ef34ebfe62d9e1540eb95679bef87735c8167969
haywhy43 <170805005@live.unilag.edu.ng>
2019-11-25 15:38:13 +0100

    .

--- app/app.js
+++ app/app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader(
         "Access-Control-Allow-Headers",
